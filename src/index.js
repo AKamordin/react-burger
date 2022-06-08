d84c419cc7e74cc4066033baefec7ab744ff1e35
Alexander Kamordin <akamordin@yandex-team.ru>
2022-06-08 22:49:56 +0300

    month-9 step-1 after 1st review

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import configureStore from './services/store'
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={configureStore}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
